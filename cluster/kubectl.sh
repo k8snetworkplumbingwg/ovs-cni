52b0de3d088b7045137cbeed19e33dc764d60fbc
Sebastian Sch <sebassch@gmail.com>
2018-08-23 17:13:08 +0300

    Added License

--- cluster/kubectl.sh
+++ cluster/kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This file is part of the KubeVirt project
+# Copyright 2018 Red Hat, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Copyright 2018 Red Hat, Inc.
 #
 
 set -e
