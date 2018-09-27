37fcd4f9e9246d4995420efbbd92188ba86e18da
Sebastian Sch <sebassch@gmail.com>
2018-09-27 12:02:45 +0300

    added functional tests for stdci.

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -2,6 +2,22 @@
 
 main() {
     echo "TODO: Add functional tests"
+    systemctl start openvswitch
+
+    cd ..
+    mkdir -p go/src/kubevirt.io
+    mkdir -p go/pkg
+
+    ln -s $(pwd)/ovs-cni go/src/kubevirt.io/
+    ls -la go/src/kubevirt.io/ovs-cni
+
+    export GOPATH=$(pwd)/go
+    cd go/src/kubevirt.io/ovs-cni
+
+    cd cmd/plugin/
+    GOOS=linux go build ./
+
+    make tests
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
