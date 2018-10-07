9a3ca476893f8ffe46474fa8e42622d3c99fddd7
Sebastian Sch <sebassch@gmail.com>
2018-10-07 12:21:35 +0300

    Added travis ci for merge

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -12,20 +12,7 @@ main() {
     cd go/src/kubevirt.io/ovs-cni
 
     echo "Run tests"
-
-    git describe --tags
-
-    if [ "$(git describe --tags --abbrev=0  --match 'v[0-9].[0-9].[0-9]' 2> /dev/null | wc -l)" == "0" ]
-    then
-        export IMAGE_TAG="master"
-    fi
-
-    if [ "$(git describe --tags --abbrev=0  --match 'v[0-9].[0-9].[0-9]' 2> /dev/null | wc -l)" == "1" ]
-    then
-        export IMAGE_TAG=`git describe --tags --abbrev=0  --match 'v[0-9].[0-9].[0-9]'`
-    fi
-
-    #make docker-build
+    make build test
 }
 
 [[ "${BASH_SOURCE[0]}" == "$0" ]] && main "$@"
