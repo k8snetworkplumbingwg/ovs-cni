068116cc4ddaec4ec147afc49ebbf1c51ef1537c
Sebastian Sch <sebassch@gmail.com>
2018-12-06 11:13:21 +0200

    Fix unit and functional tests for the plugin
    * unit tests are going to run on travis.
    * functional tests are going to run on STD-CI

--- tests/ovs_test.go
+++ tests/ovs_test.go
@@ -17,7 +17,6 @@ package tests_test
 
 import (
 	"flag"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -25,13 +24,20 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var _ = Describe("ovs-cni tests", func() {
-	Describe("pod availability tests", func() {
-		var kubeconfig *string
+var _ = Describe("ovs-cni", func() {
+	var kubeconfig *string
+	var clientset *kubernetes.Clientset
+
+	BeforeSuite(func() {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 		flag.Parse()
-		config, _ := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-		clientset, _ := kubernetes.NewForConfig(config)
+		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		Expect(err).ToNot(HaveOccurred())
+		clientset, err = kubernetes.NewForConfig(config)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Describe("pod availability tests", func() {
 		pods, _ := clientset.CoreV1().Pods("").List(v1.ListOptions{})
 		Context("pod availability tests", func() {
 			It("assert pods exists", func() {
