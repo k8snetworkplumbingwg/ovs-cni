f6d35a152e6df7274e704bd245a706640a32504e
Sebastian Sch <sebassch@gmail.com>
2019-08-28 14:21:22 +0300

    Add mtu variable
    This PR add a variable to configure the mtu on the veth interface.
    This feature is to make the ovs cni equal to the bridge cni plugin.

--- pkg/plugin/plugin_test.go
+++ pkg/plugin/plugin_test.go
@@ -34,6 +34,8 @@ import (
 
 const BRIDGE_NAME = "test-bridge"
 const VLAN_ID = 100
+const MTU = 1456
+const DEFAULT_MTU = 1500
 
 var _ = BeforeSuite(func() {
 	output, err := exec.Command("ovs-vsctl", "show").CombinedOutput()
@@ -62,7 +64,7 @@ var _ = Describe("CNI Plugin", func() {
 		Expect(err).NotTo(HaveOccurred(), "Failed to remove testing OVS bridge: %v", string(output[:]))
 	})
 
-	testAddDel := func(conf string, setVlan bool) {
+	testAddDel := func(conf string, setVlan, setMtu bool) {
 		const IFNAME = "eth0"
 
 		By("Creating temporary target namespace to simulate a container")
@@ -126,6 +128,12 @@ var _ = Describe("CNI Plugin", func() {
 			Expect(portVlan).To(Equal("[]"))
 		}
 
+		if setMtu {
+			Expect(hostLink.Attrs().MTU).To(Equal(MTU))
+		} else {
+			Expect(hostLink.Attrs().MTU).To(Equal(DEFAULT_MTU))
+		}
+
 		By("Checking that port external-id:contIface contains reference to container interface name")
 		externalIdContIface, err := getPortAttribute(hostIface.Name, "external-ids:contIface")
 		Expect(err).NotTo(HaveOccurred())
@@ -151,6 +159,12 @@ var _ = Describe("CNI Plugin", func() {
 
 			By("Checking that container interface is set UP")
 			Expect(contLink.Attrs().OperState).To(Equal(netlink.LinkOperState(netlink.OperUp)))
+
+			if setMtu {
+				Expect(contLink.Attrs().MTU).To(Equal(MTU))
+			} else {
+				Expect(contLink.Attrs().MTU).To(Equal(DEFAULT_MTU))
+			}
 
 			return nil
 		})
@@ -196,7 +210,7 @@ var _ = Describe("CNI Plugin", func() {
 				"vlan": %d
 			}`, BRIDGE_NAME, VLAN_ID)
 			It("should successfully complete ADD and DEL commands", func() {
-				testAddDel(conf, true)
+				testAddDel(conf, true, false)
 			})
 		})
 		Context("without a VLAN ID set on port", func() {
@@ -207,7 +221,19 @@ var _ = Describe("CNI Plugin", func() {
 				"bridge": "%s"
 			}`, BRIDGE_NAME)
 			It("should successfully complete ADD and DEL commands", func() {
-				testAddDel(conf, false)
+				testAddDel(conf, false, false)
+			})
+		})
+		Context("with MTU set on port", func() {
+			conf := fmt.Sprintf(`{
+				"cniVersion": "0.3.1",
+				"name": "mynet",
+				"type": "ovs",
+				"bridge": "%s",
+				"mtu": %d
+			}`, BRIDGE_NAME, MTU)
+			It("should successfully complete ADD and DEL commands", func() {
+				testAddDel(conf, false, true)
 			})
 		})
 		Context("random mac address on container interface", func() {
