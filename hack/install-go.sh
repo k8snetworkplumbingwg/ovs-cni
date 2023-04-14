bcacb237b95ab0a211f72691594c9668681eb884
Nick Rogers <ncrogers@gmail.com>
2023-04-14 12:34:31 -0400

    Plumbing to optionally build for arm64
    Signed-off-by: Nick Rogers <ncrogers@gmail.com>

--- hack/install-go.sh
+++ hack/install-go.sh
@@ -2,7 +2,8 @@
 
 destination=$1
 version=1.18.4
-tarball=go$version.linux-amd64.tar.gz
+arch="$(arch | sed s'/aarch64/arm64/' | sed s'/x86_64/amd64/')"
+tarball=go$version.linux-$arch.tar.gz
 url=https://dl.google.com/go/
 
 mkdir -p $destination
