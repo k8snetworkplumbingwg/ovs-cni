90d65bfe1ea3f7bb9ac235a4b86dcfcb7cfd40a1
oscollabus <oshoval@redhat.com>
2024-07-29 12:02:42 +0300

    Fix bump-kubevirtci.sh (#321)
    Since line format was changed to allow overriding default value,
    need to adapt the bump script.
    
    Signed-off-by: Or Shoval <oshoval@redhat.com>

--- hack/bump-kubevirtci.sh
+++ hack/bump-kubevirtci.sh
@@ -3,6 +3,6 @@
 KUBEVIRTCI_TAG=$(curl -L -Ss https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirtci/latest)
 [[ ${#KUBEVIRTCI_TAG} != "18" ]] && echo "error getting KUBEVIRTCI_TAG" && exit 1
 
-sed -i "s/export KUBEVIRTCI_TAG=.*/export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG}/g" cluster/cluster.sh
+sed -i "s/\(KUBEVIRTCI_TAG:-\)[^}]*/\1${KUBEVIRTCI_TAG}/" cluster/cluster.sh
 
 git --no-pager diff cluster/cluster.sh | grep KUBEVIRTCI_TAG || true
