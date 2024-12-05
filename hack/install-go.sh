641e6348f9a6ea234e216a84784f0e1d90c80ade
ashokpariya0 <ashok.pariya@ibm.com>
2024-12-06 03:11:38 +0530

    Add support for s390x architecture in GitHub Actions workflows. (#337)
    Updated GitHub Actions workflows to include linux/s390x in BUILD_PLATFORMS.
    Modified hack/install-go.sh to dynamically select the appropriate Go tarball
    based on the system's architecture(amd64, arm64, or others), allowing better
    support for multi-architecture environments.
    
    Signed-off-by: Ashok Pariya <ashok.pariya@ibm.com>

--- hack/install-go.sh
+++ hack/install-go.sh
@@ -2,7 +2,8 @@
 
 destination=$1
 version=$(curl -s https://go.dev/dl/?mode=json | jq -r ".[0].version")
-tarball=$version.linux-amd64.tar.gz
+arch=$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')
+tarball=$version.linux-$arch.tar.gz
 url=https://dl.google.com/go/
 
 mkdir -p $destination
