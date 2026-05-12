d88136181c86e24b77329f8a1fe50578940bd70d
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2026-05-12 18:06:17 +0200

    sriov: Refactor CmdDel
    Make the plugin/plugin.go call behave as a dispatcher, calling the right
    CmdDel function based on the device type is dealing with.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -28,7 +28,6 @@ import (
 	"github.com/containernetworking/cni/pkg/skel"
 	cnitypes "github.com/containernetworking/cni/pkg/types"
 	current "github.com/containernetworking/cni/pkg/types/100"
-	"github.com/containernetworking/plugins/pkg/ipam"
 	"github.com/containernetworking/plugins/pkg/ns"
 
 	"github.com/k8snetworkplumbingwg/ovs-cni/pkg/common"
@@ -211,92 +210,12 @@ func CmdDel(args *skel.CmdArgs) error {
 		}
 	}()
 
-	envArgs, err := common.GetEnvArgs(args.Args)
-	if err != nil {
-		return err
-	}
-
 	if !common.IsOvsHardwareOffloadEnabled(cache.Netconf.DeviceID) {
 		err = veth.CmdDel(args, cache)
 		return err
 	}
 
-	var ovnPort string
-	if envArgs != nil {
-		ovnPort = string(envArgs.OvnPort)
-	}
-	ovsDriver, err := ovsdb.NewOvsDriver(cache.Netconf.SocketFile)
-	if err != nil {
-		return err
-	}
-	bridgeName, err := sriov.GetBridgeName(ovsDriver, cache.Netconf.BrName, ovnPort, cache.Netconf.DeviceID)
-	if err != nil {
-		return err
-	}
-
-	ovsBridgeDriver, err := ovsdb.NewOvsBridgeDriver(bridgeName, cache.Netconf.SocketFile)
-	if err != nil {
-		return err
-	}
-
-	if cache.Netconf.IPAM.Type != "" {
-		err = ipam.ExecDel(cache.Netconf.IPAM.Type, args.StdinData)
-		if err != nil {
-			return err
-		}
-	}
-
-	if args.Netns == "" {
-		// The CNI_NETNS parameter may be empty according to version 0.4.0
-		// of the CNI spec (https://github.com/containernetworking/cni/blob/spec-v0.4.0/SPEC.md).
-		if common.IsOvsHardwareOffloadEnabled(cache.Netconf.DeviceID) {
-			// SR-IOV Case - The sriov device is moved into host network namespace when args.Netns is empty.
-			// This happens container is killed due to an error (example: CrashLoopBackOff, OOMKilled)
-			var rep string
-			if rep, err = sriov.GetNetRepresentor(cache.Netconf.DeviceID); err != nil {
-				return err
-			}
-			if err = common.RemoveOvsPort(ovsBridgeDriver, rep); err != nil {
-				// Don't throw err as delete can be called multiple times because of error in ResetVF and ovs
-				// port is already deleted in a previous invocation.
-				log.Printf("Error: %v\n", err)
-			}
-			// there is no network interface in case of userspace driver, so OrigIfName is empty
-			if !cache.UserspaceMode {
-				if err = sriov.ResetVF(args, cache.Netconf.DeviceID, cache.OrigIfName); err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	}
-
-	// Unlike veth pair, OVS port will not be automatically removed when
-	// container namespace is gone. Find port matching DEL arguments and remove
-	// it explicitly.
-	_, _, err = common.CleanupOvsPortBestEffort(ovsBridgeDriver, args.IfName, args.Netns)
-	if err != nil {
-		return err
-	}
-
-	if common.IsOvsHardwareOffloadEnabled(cache.Netconf.DeviceID) {
-		// there is no network interface in case of userspace driver, so OrigIfName is empty
-		if !cache.UserspaceMode {
-			err = sriov.ReleaseVF(args, cache.OrigIfName)
-			if err != nil {
-				// try to reset vf into original state as much as possible in case of error
-				if err := sriov.ResetVF(args, cache.Netconf.DeviceID, cache.OrigIfName); err != nil {
-					log.Printf("Failed best-effort cleanup of VF %s: %v", cache.OrigIfName, err)
-				}
-			}
-		}
-	}
-
-	// removes all ports whose interfaces have an error
-	if err := common.CleanPorts(ovsBridgeDriver); err != nil {
-		return err
-	}
-
+	err = sriov.CmdDel(args, cache)
 	return err
 }
 
--- pkg/sriov/plugin.go
+++ pkg/sriov/plugin.go
@@ -14,6 +14,7 @@ package sriov
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/plugins/pkg/ipam"
@@ -22,6 +23,87 @@ import (
 	"github.com/k8snetworkplumbingwg/ovs-cni/pkg/ovsdb"
 	"github.com/k8snetworkplumbingwg/ovs-cni/pkg/types"
 )
+
+func CmdDel(args *skel.CmdArgs, cache *types.CachedNetConf) error {
+	envArgs, err := common.GetEnvArgs(args.Args)
+	if err != nil {
+		return err
+	}
+
+	var ovnPort string
+	if envArgs != nil {
+		ovnPort = string(envArgs.OvnPort)
+	}
+	ovsDriver, err := ovsdb.NewOvsDriver(cache.Netconf.SocketFile)
+	if err != nil {
+		return err
+	}
+	bridgeName, err := GetBridgeName(ovsDriver, cache.Netconf.BrName, ovnPort, cache.Netconf.DeviceID)
+	if err != nil {
+		return err
+	}
+
+	ovsBridgeDriver, err := ovsdb.NewOvsBridgeDriver(bridgeName, cache.Netconf.SocketFile)
+	if err != nil {
+		return err
+	}
+
+	if cache.Netconf.IPAM.Type != "" {
+		err = ipam.ExecDel(cache.Netconf.IPAM.Type, args.StdinData)
+		if err != nil {
+			return err
+		}
+	}
+
+	// The CNI_NETNS parameter may be empty according to version 0.4.0
+	// of the CNI spec (https://github.com/containernetworking/cni/blob/spec-v0.4.0/SPEC.md).
+	if args.Netns == "" {
+		// SR-IOV Case - The sriov device is moved into host network namespace when args.Netns is empty.
+		// This happens container is killed due to an error (example: CrashLoopBackOff, OOMKilled)
+		var rep string
+		if rep, err = GetNetRepresentor(cache.Netconf.DeviceID); err != nil {
+			return err
+		}
+		if err = common.RemoveOvsPort(ovsBridgeDriver, rep); err != nil {
+			// Don't throw err as delete can be called multiple times because of error in ResetVF and ovs
+			// port is already deleted in a previous invocation.
+			log.Printf("Error: %v\n", err)
+		}
+		// there is no network interface in case of userspace driver, so OrigIfName is empty
+		if !cache.UserspaceMode {
+			if err = ResetVF(args, cache.Netconf.DeviceID, cache.OrigIfName); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	// Unlike veth pair, OVS port will not be automatically removed when
+	// container namespace is gone. Find port matching DEL arguments and remove
+	// it explicitly.
+	_, _, err = common.CleanupOvsPortBestEffort(ovsBridgeDriver, args.IfName, args.Netns)
+	if err != nil {
+		return err
+	}
+
+	// there is no network interface in case of userspace driver, so OrigIfName is empty
+	if !cache.UserspaceMode {
+		err = ReleaseVF(args, cache.OrigIfName)
+		if err != nil {
+			// try to reset vf into original state as much as possible in case of error
+			if err := ResetVF(args, cache.Netconf.DeviceID, cache.OrigIfName); err != nil {
+				log.Printf("Failed best-effort cleanup of VF %s: %v", cache.OrigIfName, err)
+			}
+		}
+	}
+
+	// removes all ports whose interfaces have an error
+	if err := common.CleanPorts(ovsBridgeDriver); err != nil {
+		return err
+	}
+
+	return err
+}
 
 func CmdCheck(args *skel.CmdArgs, netconf *types.NetConf) error {
 	envArgs, err := common.GetEnvArgs(args.Args)
